57745fb70d253d9348ad0e6516c41a38e59f8f9e
ShlomiOhana <snoop.ohana@gmail.com>
2018-06-04 17:28:41 +0300

    inbal

--- HW5/app/src/main/java/com/example/inahum/theshawshankredemption/MainActivity.java
+++ HW5/app/src/main/java/com/example/inahum/theshawshankredemption/MainActivity.java
@@ -43,20 +43,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /**sound**/
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build();
-            soundPool = new SoundPool.Builder()
-                    .setMaxStreams(10).setAudioAttributes(audioAttributes).build();
-        } else {
-            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
-        }
-        startSoundId = soundPool.load(this, R.raw.magic, 1);
-        soundPool.play(startSoundId, 1, 1, 1, 0, 1f);
-        /**sound**/
         locationClient = LocationServices
                 .getFusedLocationProviderClient(this);
 
@@ -84,39 +70,59 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("MissingPermission")
     private void startGettingLocation() {
-        locationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(5000);
+                        locationClient.getLastLocation()
+                                .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
 
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            lat = location.getLatitude();
-                            lng = location.getLongitude();
-                            /**sound**/
-                            if (Math.abs(lat - 33.237465) < 0.00005 && Math.abs(lng - 35.606734) < 0.00005) {
-                                mDataBaseHelper.setStatusByLocation();
-                                /**sound**/
-                                if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-                                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                                            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                                            .build();
-                                    soundPool = new SoundPool.Builder()
-                                            .setMaxStreams(10).setAudioAttributes(audioAttributes).build();
-                                } else {
-                                    soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
-                                }
-                                startSoundId = soundPool.load(MainActivity.this, R.raw.magic, 1);
-                                soundPool.play(startSoundId, 1, 1, 1, 0, 1f);
-                                /**sound**/
-                            }
-                            Toast.makeText(MainActivity.this,
-                                    "lat: " + lat + ", lng: " + lng,
-                                    Toast.LENGTH_LONG).show();
-                        }
+                                        // Got last known location. In some rare situations this can be null.
+                                        if (location != null) {
+                                            lat = location.getLatitude();
+                                            lng = location.getLongitude();
+
+                                            if (Math.abs(lat - 33.237465) < 0.00005 && Math.abs(lng - 35.606734) < 0.00005) {
+                                                mDataBaseHelper.setStatusByLocation();
+                                                populateListView();
+                                                /**sound**/
+                                                if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+                                                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                                                            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                                                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                                                            .build();
+                                                    soundPool = new SoundPool.Builder()
+                                                            .setMaxStreams(10).setAudioAttributes(audioAttributes).build();
+                                                } else {
+                                                    soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+                                                }
+                                                startSoundId = soundPool.load(MainActivity.this, R.raw.magic, 1);
+                                                soundPool.play(startSoundId, 1, 1, 1, 0, 1f);
+                                                /**sound**/
+                                            }
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    Toast.makeText(MainActivity.this,
+                                                            "lat: " + lat + ", lng: " + lng,
+                                                            Toast.LENGTH_LONG).show();
+                                                }
+                                            });
+
+                                        }
+                                    }
+                                });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                });
+                }
+            }
+        }).start();
+
     }
 
     @Override
